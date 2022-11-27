e5bf02dbd7d186baaa44a56f9660b0d537fba7a2
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-11-27 15:20:34 +0200

    Add project files.

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+
+namespace lab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex 5
+                Se citesc doua numere de la tastatura, x,y.Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+                • Exemplu: citim 9,0 Afisam: 0 9 */
+            Console.WriteLine("Introduceti primul numar:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti al doilea numar:");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            { 
+                Console.WriteLine("Numbers in order: "+ y + ' ' + x);
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Numbers in order: "+ x + ' ' + y);
+            }    
+          
+
+        }
+    }
+}
